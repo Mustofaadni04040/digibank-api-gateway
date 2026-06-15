547895d10858550af58d42ccea2b17da1078cd65
Mustofa Adny <adniadni04@gmail.com>
2026-06-15 15:10:05 +0700

    feat: create security config & API routing

---
+++ src/main/java/com/example/apigateway/security/GatewaySecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.apigateway.security;
+
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+       return httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+               .authorizeExchange(authorizeExchangeSpec ->
+                       authorizeExchangeSpec.anyExchange().permitAll()).build(); // allow global filter to handle authentication
+    }
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-account-service", r -> r.path("/api/auth/**", "/api/users/**", "/api/accounts/**")
+                        .uri("lb://USER-ACCOUNT-SERVICE")
+                )
+                .route("transaction-service", r -> r.path("/api/transaction/**")
+                        .uri("lb://TRANSACTION-SERVICE")
+                )
+                .build();
+
+    }
+}
